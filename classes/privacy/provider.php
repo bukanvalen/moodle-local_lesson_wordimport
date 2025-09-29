3160f0d3fae542e40283cd5a250ff47e25070a65
Valentino Harpa <67551202+bukanvalen@users.noreply.github.com>
2025-09-29 10:24:08 +0700

    Update provider.php to fix namespace error
    Applying the fix from this issue: https://github.com/ecampbell/moodle-local_lesson_wordimport/issues/3

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -32,7 +32,7 @@ namespace local_lesson_wordimport\privacy;
  */
 class provider implements \core_privacy\local\metadata\null_provider {
 
-    use core_privacy\local\legacy_polyfill;
+    use \core_privacy\local\legacy_polyfill;
 
     /**
      * Get the language string identifier with the component's language
